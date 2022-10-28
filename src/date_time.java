237a7df11571fa7e3399065ade79ab8f95ffd9f9
roboanonymous <jsamyak591@gmail.com>
2022-10-28 14:42:56 +0530

    first

--- src/date_time.java
+++ src/date_time.java
@@ -1,14 +1,19 @@
 import java.time.*;
+import java.util.*;
 public class date_time {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(System.currentTimeMillis());
-		System.out.println(System.currentTimeMillis()/1000);
-		System.out.println(System.currentTimeMillis()/1000/60);
-		System.out.println(System.currentTimeMillis()/1000/3600);
-		System.out.println(System.currentTimeMillis()/1000/3600/24);
-		System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+//		System.out.println(System.currentTimeMillis());
+//		System.out.println(System.currentTimeMillis()/1000);
+//		System.out.println(System.currentTimeMillis()/1000/60);
+//		System.out.println(System.currentTimeMillis()/1000/3600);
+//		System.out.println(System.currentTimeMillis()/1000/3600/24);
+//		System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+		
+		Date d = new Date();
+		System.out.println(d);
+		System.out.println(d.getDate());
 	}
 
 }
