2903269016bd04a85e88ab688c5dd5c38ba17ef4
roboanonymous <jsamyak591@gmail.com>
2022-10-28 13:36:16 +0530

    first commit

---
+++ src/date_time.java
@@ -0,0 +1,14 @@
+import java.time.*;
+public class date_time {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(System.currentTimeMillis());
+		System.out.println(System.currentTimeMillis()/1000);
+		System.out.println(System.currentTimeMillis()/1000/60);
+		System.out.println(System.currentTimeMillis()/1000/3600);
+		System.out.println(System.currentTimeMillis()/1000/3600/24);
+		System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+	}
+
+}
